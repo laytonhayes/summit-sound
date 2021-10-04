5cf178a91d57a0246ce765da196228e6a7786289
Layton Hayes <lhayes@indeed.com>
2021-10-04 08:48:20 -0500

    Added volumn contol type config

--- plugins/spotify/start.sh
+++ plugins/spotify/start.sh
@@ -10,6 +10,7 @@ fi
 # SOUND_SPOTIFY_BITRATE: Set the playback bitrate
 SOUND_DEVICE_NAME=${SOUND_DEVICE_NAME:-"balenaSound Spotify $(echo "$BALENA_DEVICE_UUID" | cut -c -4)"}
 SOUND_SPOTIFY_BITRATE=${SOUND_SPOTIFY_BITRATE:-160}
+SOUND_SPOTIFY_VOLUME_CTRL=${SOUND_SPOTIFY_VOLUME_CTRL:-linear}
 
 # SOUND_SPOTIFY_DISABLE_NORMALISATION: Disable volume normalisation
 if [[ -z "$SOUND_SPOTIFY_DISABLE_NORMALISATION" ]]; then
@@ -43,7 +44,7 @@ set -- /usr/bin/librespot \
   --name "$SOUND_DEVICE_NAME" \
   --bitrate "$SOUND_SPOTIFY_BITRATE" \
   --cache /var/cache/raspotify \
-  --volume-ctrl linear \
+  --volume-ctrl "SOUND_SPOTIFY_VOLUME_CTRL" \
   "$@"
 
 exec "$@"
